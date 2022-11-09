bd169d5e635e6473010c7322fa245cbcb486d362
ThunderGer <Jaames11@hotmail.com>
2022-11-08 19:43:27 -0600

    Agregando boton

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-export const App = () => {
-  return (
-    <h1>App</h1>
+export const App = ({texto}) => {
+  return(
+    <>
+      <div className="d-grid gap-2 mb-4">
+          <button className="btn btn-light" type="button">
+              <i className="fa fa-right-to-bracket orange"></i>
+              {texto}
+          </button>
+      </div>
+    </>
   )
 }
