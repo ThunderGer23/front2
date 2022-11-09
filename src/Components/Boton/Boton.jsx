38093540a799f369a0a5efa5b1983b01f0c079f2
ThunderGer <jaames11@hotmail.com>
2022-11-09 11:09:42 -0600

    Test boton para el div

--- src/Components/Boton/Boton.jsx
+++ src/Components/Boton/Boton.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 export const Boton = ({texto}) => {
   return(
     <>
-      <div className="d-grid gap-2 mb-4">
+      <div role='div' className="d-grid gap-2 mb-4">
           <button className="btn btn-light" type="button">
-              <i className="fa fa-right-to-bracket orange"></i>
+              <i role='i' className="fa fa-right-to-bracket orange"></i>
               {texto}
           </button>
       </div>
--- test/Components/Boton/Boton.test.jsx
+++ test/Components/Boton/Boton.test.jsx
@@ -1,4 +1,4 @@
-import {render,screen} from "@testing-library/react"
+import {render, screen} from "@testing-library/react"
 import { Boton } from "../../../src/Components/Boton/Boton"
 
 describe('Pruebas en el Boton',()=>{
@@ -11,12 +11,14 @@ describe('Pruebas en el Boton',()=>{
 
     test('Esta prueba valida que el div exista y pertenezca a la clase correcta',() =>{
         render(<Boton texto={texto}/>)
-        //expect(screen.getAllByRole('div')).toBeTruthy()//Se valida que exista el div
-        const {container} = screen.getAllByRole('div')
-        const {className}= screen.getByRole('div')//Se salva la clase del div del componente en className
-        console.log(container)
+        expect(screen.getAllByRole('div')).toBeTruthy()//Se valida que exista el div
+        const {className}= screen.getByRole('div')//Se salva la clase del div del componente en className        
         expect(className).toBe('d-grid gap-2 mb-4')//Se valida que el div tenga la misma clase que className
     })
 
+    test('Esta prueba valida que la etiqueta i exista y pertenezca a la clase correcta',() =>{
+        render(<Boton texto={texto}/>)
+        expect(screen.getAllByRole('i')).toBeTruthy()//Se valida que exista el div
+    })
     
 })
