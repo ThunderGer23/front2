4ff1a591fd02adc9fbfbf1236871c1c8a6bc9b36
unknown <atoledoa1300@alumno.ipn.mx>
2022-11-09 16:22:06 -0600

    Fin de pruebas del Boton (por ahora)

--- test/Components/Boton/Boton.test.jsx
+++ test/Components/Boton/Boton.test.jsx
@@ -11,14 +11,23 @@ describe('Pruebas en el Boton',()=>{
 
     test('Esta prueba valida que el div exista y pertenezca a la clase correcta',() =>{
         render(<Boton texto={texto}/>)
-        expect(screen.getAllByRole('div')).toBeTruthy()//Se valida que exista el div
+        expect(screen.getByRole('div')).toBeTruthy()//Se valida que exista el div
         const {className}= screen.getByRole('div')//Se salva la clase del div del componente en className        
         expect(className).toBe('d-grid gap-2 mb-4')//Se valida que el div tenga la misma clase que className
     })
 
+    test('Esta prueba valida que el button exista y pertenezca a la clase correcta', () =>{
+        render(<Boton texto={texto}/>)
+        expect(screen.getByRole('button')).toBeTruthy()
+        const{className}=screen.getByRole('button')
+        expect(className).toBe('btn btn-light')
+    })
+
     test('Esta prueba valida que la etiqueta i exista y pertenezca a la clase correcta',() =>{
         render(<Boton texto={texto}/>)
-        expect(screen.getAllByRole('i')).toBeTruthy()//Se valida que exista el div
+        expect(screen.getByRole('i')).toBeTruthy()//Se valida que exista la eiqueta i
+        const{className}=screen.getByRole('i')
+        expect(className).toBe('fa fa-right-to-bracket orange')
     })
     
 })
